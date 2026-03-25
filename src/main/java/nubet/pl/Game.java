fbe5cd0520a1426b40ecedce52ca047c2bda0f43
Norbert Fila <nubet2137@gmail.com>
2026-03-25 15:53:38 +0100

    feat: implement basic score tracking in Game class (GREEN)

--- src/main/java/nubet/pl/Game.java
+++ src/main/java/nubet/pl/Game.java
@@ -1,12 +1,14 @@
 package nubet.pl;
 
 public class Game {
-    public void roll(int pins) {
+    private int score = 0;
 
+    public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
 
