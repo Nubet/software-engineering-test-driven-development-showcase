f1b2697d019b85d1354c29537643f41024fb097b
Norbert Fila <nubet2137@gmail.com>
2026-03-26 07:29:01 +0100

    feat: implement strike scoring in Game class (GREEN)

--- src/main/java/nubet/pl/Game.java
+++ src/main/java/nubet/pl/Game.java
@@ -1,14 +1,27 @@
 package nubet.pl;
 
 public class Game {
-    private int score = 0;
+    private final int[] rolls = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
+        int score = 0;
+        int cursor = 0;
+
+        for (int frame = 0; frame < 10; frame++) {
+            if (rolls[cursor] == 10) {
+                score += 10 + rolls[cursor + 1] + rolls[cursor + 2];
+                cursor += 1;
+            } else {
+                score += rolls[cursor] + rolls[cursor + 1];
+                cursor += 2;
+            }
+        }
+
         return score;
     }
 }
-
